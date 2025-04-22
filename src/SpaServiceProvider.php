f1c917ed9570881f1b39ad6d1851532e615255af
Guenther Kron <kron@naturwelt.at>
2025-04-22 08:21:20 +0200

    methods

--- src/SpaServiceProvider.php
+++ src/SpaServiceProvider.php
@@ -8,7 +8,7 @@ use Itstudioat\Spa\Commands\CreateUser;
 use Itstudioat\Spa\Commands\InstallMe;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
+
 
 class SpaServiceProvider extends PackageServiceProvider
 {
@@ -23,7 +23,7 @@ class SpaServiceProvider extends PackageServiceProvider
             ->name('spa');
     }
 
-    public function packageRegistered()
+    public function registeringPackage()
     {
         $this->publishes([
             __DIR__ . '/../config/spa.php' => config_path('spa.php'),
@@ -44,23 +44,21 @@ class SpaServiceProvider extends PackageServiceProvider
             __DIR__ . '/../src/Notifications' => app_path('/Notifications'),
             __DIR__ . '/../src/Services' => app_path('/Services'),
             __DIR__ . '/../src/Traits' => app_path('/Traits'),
-        ], 'app');
+        ], 'aps-app');
 
         $this->publishes([
             __DIR__ . '/../stubs/vite.config.js' => base_path(),
-        ], 'stubs');
-
-
+        ], 'spa-stubs');
 
         $this->publishesMigrations([
             __DIR__ . '/../database/migrations' => database_path('migrations'),
-        ], 'migrations');
+        ], 'spa-migrations');
 
         $this->publishes([
             __DIR__ . '/../config/spa.php' => config_path('spa.php'),
             __DIR__ . '/../resources' => resource_path(),
             __DIR__ . '/../routes' => base_path('/routes'),
-            __DIR__ . '/../src/commands' => app_path('/commands'),
+            __DIR__ . '/../src/Commands' => app_path('/commands'),
             __DIR__ . '/../src/Commands' => app_path('/Commands'),
             __DIR__ . '/../src/Http' => app_path('/Http'),
             __DIR__ . '/../src/Models' => app_path('/Models'),
@@ -68,8 +66,10 @@ class SpaServiceProvider extends PackageServiceProvider
             __DIR__ . '/../src/Services' => app_path('/Services'),
             __DIR__ . '/../src/Traits' => app_path('/Traits'),
             __DIR__ . '/../stubs/vite.config.js' => base_path(),
-        ], 'all');
+        ], 'spa-all');
     }
+
+    public function packageRegistered() {}
 
     public function bootingPackage()
     {
