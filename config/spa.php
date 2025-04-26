6dad25f9bf08d5af09f67c46573756c52089752f
Guenther Kron <kron@naturwelt.at>
2025-04-27 00:34:13 +0200

    update Profile

--- config/spa.php
+++ config/spa.php
@@ -2,7 +2,7 @@
 
 // config for Itstudioat/Spa
 return [
-    'version' => '0.0.3',
+    'version' => '0.0.4',
     'logo' => 'logo.png',
     'copyright' => '(c) 2025 ITStudio.at by Günther Kron',
     'timeout' => 3000,
